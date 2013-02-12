60c5581c13486d6d1ce65896e12288afc522dadb
Daniel Cousens <daniel210x@gmail.com>
2013-02-12 07:28:57 +0000

    adds smarter symbolic linking for dot-install and vim-install
    The process now uses the interactive flag '-i' instead of automatically deleting defaults.
    vim-install also stops .vim recursing on itself by using the the '-n' option for directories.
    
    For further information see `man ln`.

--- dot-install.sh
+++ dot-install.sh
@@ -2,17 +2,7 @@
 
 wd=`pwd`
 
-if [ ! -f $wd/setup.sh ]; then
-	echo "Must execute in same working directory."
-	exit
-fi
-
-echo "Deleting defaults."
-rm ~/.bash* -f
-rm ~/.gitconfig -f
-
-echo "Creating symbolic links."
-ln -s $wd/.bashrc ~/.bashrc
-ln -s $wd/.bash_profile ~/.bash_profile
-ln -s $wd/.gitconfig ~/.gitconfig
-ln -s $wd/.xinitrc ~/.xinitrc
+ln -si $wd/.bash_profile ~/.bash_profile
+ln -si $wd/.bashrc ~/.bashrc
+ln -si $wd/.gitconfig ~/.gitconfig
+ln -si $wd/.xinitrc ~/.xinitrc
--- vim-install.sh
+++ vim-install.sh
@@ -2,19 +2,9 @@
 
 wd=`pwd`
 
-if [ ! -f $wd/setup.sh ]; then
-	echo "Must execute in same working directory."
-	exit
-fi
-
 while [ ! -f /usr/bin/vim ]; do
 	yaourt -S vim
 done
 
-echo "Deleting defaults."
-rm ~/.vim -rf
-rm ~/.vimrc -f
-
-echo "Creating symbolic links."
-ln -s $wd/.vim/ ~/.vim
-ln -s $wd/.vimrc ~/.vimrc
+ln -sin $wd/.vim/ ~/.vim
+ln -si $wd/.vimrc ~/.vimrc
