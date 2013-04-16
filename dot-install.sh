221970ecf72f0991fb38b4649f6a5ab5af249220
Daniel Cousens <daniel210x@gmail.com>
2013-04-17 05:08:48 +1000

    removes redundant vim check

--- dot-install.sh
+++ dot-install.sh
@@ -2,10 +2,6 @@
 
 wd=`pwd`
 
-while [ ! -f /usr/bin/vim ]; do
-	yaourt -S vim
-done
-
 ln -si $wd/.bash_aliases ~/.bash_aliases
 ln -si $wd/.bash_profile ~/.bash_profile
 ln -si $wd/.bashrc ~/.bashrc
