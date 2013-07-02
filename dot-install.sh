714b3bf7d0eb51c65f34c0d10ae7804392fd97a5
Daniel Cousens <daniel210x@gmail.com>
2013-07-02 20:31:11 +1000

    Removes irssi and rtorrent by default

--- dot-install.sh
+++ dot-install.sh
@@ -6,8 +6,6 @@ ln -si $wd/.bash_profile ~/.bash_profile
 ln -si $wd/.bashrc ~/.bashrc
 ln -si $wd/.gitconfig ~/.gitconfig
 ln -si $wd/.inputrc ~/.inputrc
-ln -sin $wd/.irssi/ ~/.irssi
-ln -si $wd/.rtorrent.rc ~/.rtorrent.rc
 ln -sin $wd/.vim/ ~/.vim
 ln -si $wd/.vimrc ~/.vimrc
 ln -si $wd/.xinitrc ~/.xinitrc
